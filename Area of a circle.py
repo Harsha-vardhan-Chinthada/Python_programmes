46fb00e9fa574a2b3afc0b3a80334efca348893d
Harsha-vardhan-Chinthada <113857603+Harsha-vardhan-Chinthada@users.noreply.github.com>
2022-11-04 22:42:22 +0530

    Add files via upload

---
+++ Area of a circle.py
@@ -0,0 +1,11 @@
+#Area of circle using formula
+r=float(input("Radius of the circle: "))
+pi=22/7
+area= pi*r**2
+print("The area of circle is: ",area)
+
+#Using Math Module
+import math
+r= float(input("Radius of the circle is: "))
+area= math.pi*r**2
+print("Area of the circle is: ",area)
