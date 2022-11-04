c7378f03277870ad50dafc859cb1bdf6755fac47
Harsha-vardhan-Chinthada <113857603+Harsha-vardhan-Chinthada@users.noreply.github.com>
2022-11-04 22:43:02 +0530

    Add files via upload

---
+++ Students details.py
@@ -0,0 +1,6 @@
+studentid= int(input("Enter student id: "))
+Name= input("Enter student Name: ")
+Marks= float(input("Enter Marks scored: "))
+print("ID: ",studentid)
+print("Name: ",Name)
+print("Marks scored: ",Marks)
